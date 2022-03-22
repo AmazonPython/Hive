d54b70e7b6c9be5461d1b2d4a408e6daf878b9be
AmazonPython <amazon3@foxmail.com>
2022-03-22 21:09:02 +0800

    The basic layout is designed. ✅

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-start">
+            Design and Code by <a href="https://github.com/AmazonPython" target="_blank"><span style="color: #8a2be2">Nova</span></a> with <span style="color: #e27575;">❤</span>
+        </p>
+
+        <p class="float-end"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
