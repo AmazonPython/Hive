3624ee12a96f2348e715972a685730be7c33963a
AmazonPython <amazon3@foxmail.com>
2022-03-23 21:10:03 +0800

    Added create user address function, and localize validate messages. ✅ 🌐

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css')
-    .version();
+    .sass('resources/sass/app.scss', 'public/css');
