079552084b40b03f191c9f1216f1580fb65f00e6
AmazonPython <amazon3@foxmail.com>
2022-03-26 17:38:19 +0800

    Added user list of administrator background. ✅

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,7 +10,7 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
     'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
-
     $router->get('/', 'HomeController@index')->name('home');
 
+    $router->get('users', 'UserController@index');
 });
