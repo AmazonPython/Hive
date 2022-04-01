49207e843229136b77a18044450b97d3e8f4c4e2
AmazonPython <amazon3@foxmail.com>
2022-04-01 23:57:38 +0800

    Added product shopping cart function. 👔

--- routes/web.php
+++ routes/web.php
@@ -43,4 +43,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     // 收藏商品列表
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
+    // 添加购物车
+    Route::post('cart', 'CartController@add')->name('cart.add');
 });
