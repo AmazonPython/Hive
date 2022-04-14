0541aa6f0150136003dcf4ed44b8d52776bf56d4
AmazonPython <amazon3@foxmail.com>
2022-04-14 19:49:45 +0800

    Improve payment callback. ✅

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Exceptions\InvalidRequestException;
 
 class PaymentController extends Controller
 {
-    public function payByAliPay(Order $order, Request $request)
+    public function payByAlipay(Order $order, Request $request)
     {
         // 判断订单是否属于当前用户
         $this->authorize('own', $order);
@@ -41,7 +42,35 @@ class PaymentController extends Controller
     // 服务器端回调页面
     public function alipayNotify()
     {
-        $date = app('alipay')->verify();
-        \Log::debug('Alipay notify', $date->all());
+        // 校验输入参数
+        $data  = app('alipay')->verify();
+
+        // 如果订单状态不是成功或者结束，则不走后续的逻辑
+        // 所有交易状态：https://docs.open.alipay.com/59/103672
+        if(!in_array($data->trade_status, ['TRADE_SUCCESS', 'TRADE_FINISHED'])) {
+            return app('alipay')->success();
+        }
+
+        // $data->out_trade_no 拿到订单流水号，并在数据库中查询
+        $order = Order::where('no', $data->out_trade_no)->first();
+
+        // 正常来说不太可能出现支付了一笔不存在的订单，这个判断只是加强系统健壮性。
+        if (!$order) {
+            return 'fail';
+        }
+
+        // 如果这笔订单的状态已经是已支付
+        if ($order->paid_at) {
+            // 返回数据给支付宝
+            return app('alipay')->success();
+        }
+
+        $order->update([
+            'paid_at'        => Carbon::now(), // 支付时间
+            'payment_method' => 'alipay', // 支付方式
+            'payment_no'     => $data->trade_no, // 支付宝订单号
+        ]);
+
+        return app('alipay')->success();
     }
 }
