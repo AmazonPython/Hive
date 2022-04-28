aa1bc50b0711708aa22cd3ab634cdbaf6572e9a0
AmazonPython <amazon3@foxmail.com>
2022-04-28 20:55:34 +0800

    Added create coupon code function of administrator background. ✨ ✅ 👔

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -33,4 +33,8 @@ Route::group([
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('orders.handle_refund');
 
     $router->get('coupon_codes', 'CouponCodesController@index');
+
+    $router->post('coupon_codes', 'CouponCodesController@store');
+
+    $router->get('coupon_codes/create', 'CouponCodesController@create');
 });
