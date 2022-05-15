6266c726268dd9b798878d912606baf9978991ff
AmazonPython <amazon3@foxmail.com>
2022-05-15 19:50:55 +0800

    The category field is added in the product function of administrator background. 🎨 ✨ 👔 👽️

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -29,6 +30,9 @@ class ProductFactory extends Factory
             "https://cdn.learnku.com/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
         ]);
 
+        // 从数据库中随机取一个类目
+        $category = Category::query()->where('is_directory', false)->inRandomOrder()->first();
+
         return [
             'title' => $this->faker->word,
             'description' => $this->faker->sentence,
@@ -38,6 +42,10 @@ class ProductFactory extends Factory
             'sold_count' => 0,
             'review_count' => 0,
             'price' => 0,
+
+            // 将取出的类目 ID 赋给 category_id 字段
+            // 如果数据库中没有类目，则 $category 为 null，这里会自动跳过。同理 category_id 字段也会自动跳过。
+            'category_id' =>  $category ? $category->id : null,
         ];
     }
 }
