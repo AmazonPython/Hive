d8e976e7aa9424a3c340d7eff9d2b0c57625e8e9
AmazonPython <amazon3@foxmail.com>
2022-06-06 20:14:36 +0800

    Update README.md and test related files. ✅ 📝

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        // 执行数据库迁移
+        $this->artisan('migrate');
+    }
 }
